c4d3c3ee76b02a0b0f20e60fb5abcc16f7885a6d
Jupilian <ropersjulien@gmail.com>
2020-12-08 18:48:34 +0100

    Initial Commit

---
+++ Password generator/Generator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Password_generator
+{
+    public class Generator
+    {
+        
+        public static string Generate()
+        {
+            if (MainWindow.Security == 0)
+                return Password(6, false, false);
+            else if (MainWindow.Security == 1)
+                return Password(8, true, false);
+            else if (MainWindow.Security == 2)
+                return Password(10, true, true);
+            else
+                return "";
+        }
+
+
+        private static Random rng = new Random();
+        private static string Password(int length, bool alphaNum, bool specials)
+        {
+
+            var stringChars = new char[length];
+            var characters =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz";
+            
+            if(alphaNum == true)
+            {
+                characters = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxy0123456789";
+            }
+
+            if(specials == true)
+            {
+                characters = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxy0123456789$àç@#_/";
+            }
+            
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = characters[rng.Next(characters.Length)];
+            }
+
+            string password;
+            password = new String(stringChars);
+            return password;
+
+        }
+        
+        
+        
+    }
+}
---
+++ Password generator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Controls;
+
+namespace Password_generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public static int Security = 0;
+        
+        
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Result.Text = Generator.Generate();
+        }
+
+        private void RadioButton_Checked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Security = 0;
+        }
+
+        private void RadioButton_Checked_1(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Security = 1;
+        }
+
+        private void RadioButton_Checked_2(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Security = 2;
+        }
+    }
+}
